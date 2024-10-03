05e21c6b94db8b65f9d539ecf60558619408068d
Remy Saguez <resaguez.2800@gmail.com>
2024-10-03 14:47:23 +0200

    entiter

---
+++ src/Entity/PlaylistMedia.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlaylistMediaRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PlaylistMediaRepository::class)]
+class PlaylistMedia
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $added_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAddedAt(): ?\DateTimeInterface
+    {
+        return $this->added_at;
+    }
+
+    public function setAddedAt(\DateTimeInterface $added_at): static
+    {
+        $this->added_at = $added_at;
+
+        return $this;
+    }
+}
---
+++ src/Entity/PlaylistSubscription.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlaylistSubscriptionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PlaylistSubscriptionRepository::class)]
+class PlaylistSubscription
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $subscribed_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSubscribedAt(): ?\DateTimeInterface
+    {
+        return $this->subscribed_at;
+    }
+
+    public function setSubscribedAt(\DateTimeInterface $subscribed_at): static
+    {
+        $this->subscribed_at = $subscribed_at;
+
+        return $this;
+    }
+}
