97a1cca590f94b8d474f543108d3a101ba891fb2
Remy Saguez <resaguez.2800@gmail.com>
2024-10-30 16:55:58 +0100

    cours 30/10/24

--- src/Controller/Other/SubscriptionController.php
+++ src/Controller/Other/SubscriptionController.php
@@ -4,15 +4,22 @@ declare(strict_types=1);
 
 namespace App\Controller\Other;
 
+use App\Repository\SubscriptionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SubscriptionController extends AbstractController
 {
-    #[Route(path: '/subscriptions' , name: 'subscriptions')]
-    public function show(): Response
+    #[Route(path: '/subscriptions', name: 'subscriptions')]
+    public function show(
+        SubscriptionRepository $subscriptionRepository
+    ): Response
     {
-        return $this->render('other/abonnements.html.twig');
+        $subscriptions = $subscriptionRepository->findAll();
+
+        return $this->render('other/abonnements.html.twig', [
+            'subscriptions' => $subscriptions,
+        ]);
     }
 }
